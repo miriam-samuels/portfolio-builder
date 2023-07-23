3eb3c5401aa6b1a7a5dbdfa72a8f3865d8a9f5b3
miriam-samuels <medomiriam@gmail.com>
2023-07-23 23:40:13 +0100

    create table if it does not exist

--- src/database/connection.go
+++ src/database/connection.go
@@ -26,6 +26,42 @@ func ConnectDB() {
 	// CurrentDb()
 	fmt.Println("Connection to DB Successful")
 	Db.SetConnMaxLifetime(time.Minute * 3)
+
+	createTable()
+}
+
+func createTable() {
+	var exists bool
+	err := Db.QueryRow("SELECT * FROM").Scan(&exists)
+	if err != nil {
+		_, err := Db.Exec(`
+		CREATE TABLE users(
+			id VARCHAR(40) PRIMARY KEY,
+			username VARCHAR(30) NOT NULL ,
+			password VARCHAR(100) NOT NULL,
+			email VARCHAR(45) DEFAULT '',
+			first_name VARCHAR(45) DEFAULT '',
+			last_name VARCHAR(45) DEFAULT '',
+			phone VARCHAR(20) DEFAULT '',
+			github VARCHAR(45) DEFAULT '',
+			medium VARCHAR(45) DEFAULT '',
+			twitter VARCHAR(45) DEFAULT '',
+			linkedin VARCHAR(45) DEFAULT '',
+			objective VARCHAR(400) DEFAULT '',
+			tagline VARCHAR(150) DEFAULT '',
+			skills JSON,
+			projects JSON,
+			theme VARCHAR(30) DEFAULT ''
+		);
+		`)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Table created successfully.")
+	}
+
 }
 
 func GetDb() *sql.DB {
